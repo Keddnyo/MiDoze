bb7465a34c3703e7d0cfe56a0adf1b19a73738e1
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-04-27 07:48:08 +0300

    Update DozeRequest.kt

--- app/src/main/java/io/github/keddnyo/midoze/utils/DozeRequest.kt
+++ app/src/main/java/io/github/keddnyo/midoze/utils/DozeRequest.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.app.Activity
 import android.app.DownloadManager
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
