96c09d44fa5adbf1c1061687702b449eefb04dab
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-07-09 01:38:31 +0300

    Misc

--- app/src/main/java/io/github/keddnyo/midoze/fragments/FeedFragment.kt
+++ app/src/main/java/io/github/keddnyo/midoze/fragments/FeedFragment.kt
@@ -22,13 +22,10 @@ import com.google.gson.reflect.TypeToken
 import io.github.keddnyo.midoze.R
 import io.github.keddnyo.midoze.utils.DozeRequest
 import io.github.keddnyo.midoze.utils.StringUtils
-import io.github.keddnyo.midoze.utils.TinyDB
 import io.github.keddnyo.midoze.utils.UiUtils
 import io.github.keddnyo.midoze.utils.devices.DeviceData
 import io.github.keddnyo.midoze.utils.firmwares.FirmwaresAdapter
 import io.github.keddnyo.midoze.utils.firmwares.FirmwaresData
-import org.json.JSONArray
-import org.json.JSONObject
 
 class FeedFragment : Fragment() {
 
@@ -58,7 +55,6 @@ class FeedFragment : Fragment() {
         val firmwaresRefreshLayout: SwipeRefreshLayout = findViewById(R.id.firmwaresRefreshLayout)
 
         val firmwaresProgressBar: ProgressBar = findViewById(R.id.firmwaresProgressBar)
-        val firmwaresLoadingDataText: TextView = findViewById(R.id.firmwaresLoadingDataText)
 
         val firmwaresErrorMessage: ConstraintLayout = findViewById(R.id.firmwaresErrorMessage)
 
@@ -75,34 +71,22 @@ class FeedFragment : Fragment() {
         class LoadDataForActivity :
             AsyncTask<Void?, Void?, Void>() {
 
-            val tinydb = TinyDB(context)
             val gson = Gson()
             var deviceArrayList: ArrayList<DeviceData> = arrayListOf()
 
             var appName = ""
             var appVersion: String? = ""
 
-            var firmwaresData: JSONObject = JSONObject("{}")
-            val preloadedFirmwares = prefs.getString("Firmwares", "").toString()
-
             @Deprecated("Deprecated in Java")
             override fun onPreExecute() {
                 super.onPreExecute()
                 firmwaresProgressBar.visibility = View.VISIBLE
-                firmwaresLoadingDataText.visibility = View.VISIBLE
                 firmwaresErrorMessage.visibility = View.GONE
                 firmwaresRefreshLayout.isRefreshing = false
             }
 
             @Deprecated("Deprecated in Java")
             override fun doInBackground(vararg p0: Void?): Void? {
-                /*val deviceArrayListObject = tinydb.getListObject("deviceArrayList", DeviceData::class.java)
-
-                deviceArrayListObject.forEach {
-                    deviceArrayList.add(it as DeviceData)
-                }*/
-
-
 
                 fun getAppName(): String? {
                     return prefs.getString("settings_feed_app_name", "Zepp")
@@ -124,7 +108,6 @@ class FeedFragment : Fragment() {
                     }
                 }
 
-
                 fun getFirmwaresData() {
                     val isOnline = DozeRequest().isOnline(context)
 
@@ -134,17 +117,6 @@ class FeedFragment : Fragment() {
                         val jsArray = gson.toJson(deviceArrayList)
                         editor.putString("deviceArrayList", jsArray.toString())
                         editor.apply()
-
-                        /*val json2 = gson.toJson(deviceArrayList)
-                        editor.putString("deviceArrayList", json2)
-                        editor.apply()*/
-
-                        /*deviceArrayListObject.clear()
-                        deviceArrayList.forEach {
-                            deviceArrayListObject.add(it as Any)
-                        }
-
-                        tinydb.putListObject("deviceArrayList", deviceArrayListObject)*/
                     } else {
                         runOnUiThread {
                             firmwaresProgressBar.visibility = View.GONE
@@ -174,79 +146,30 @@ class FeedFragment : Fragment() {
             override fun onPostExecute(result: Void?) {
                 super.onPostExecute(result)
 
-                deviceArrayList.forEachIndexed { index, it ->
-                    firmwaresAdapter.addDevice(
-                        FirmwaresData(
-                            it.name,
-                            it.icon,
-                            StringUtils().getLocalFirmwareDate(it.buildTime),
-                            it.firmware.getString("firmwareVersion").toString(),
-                            it.deviceSource.toInt(),
-                            it.productionSource.toInt(),
-                            appName,
-                            appVersion.toString()
-                        )
-                    )
-                    firmwaresAdapter.notifyItemInserted(index)
-                    deviceListIndex[it.name] = index
-                }
-
-                firmwaresProgressBar.visibility = View.GONE
-                firmwaresLoadingDataText.visibility = View.GONE
-
-                /*fun getData(favorite: Boolean) {
-                    if (firmwaresData != JSONObject("{}")) {
-
-                        val responseParamsArray = firmwaresData.toMap()
-                        val keys = responseParamsArray.keys
-
-                        for (i in keys) {
-                            val jsonObject = firmwaresData.getJSONObject(i)
-
-                            val deviceNameValue = jsonObject.getString("name")
-                            val deviceIconValue = when {
-                                deviceNameValue.contains(getString(R.string.title_mi_band),
-                                    true) -> {
-                                    R.drawable.ic_xiaomi
-                                }
-                                deviceNameValue.contains(getString(R.string.title_zepp), true) -> {
-                                    R.drawable.ic_zepp
-                                }
-                                else -> {
-                                    R.drawable.ic_amazfit
-                                }
-                            }
-                            val firmwareVersionValue = jsonObject.getString("fw")
-                            val firmwareReleaseDateValue =
-                                StringUtils().getLocaleFirmwareDate(jsonObject.getString("date"))
-
-                            val firmwareVersion = getString(R.string.firmware_version)
-                            val firmwareChangelogValue = "$firmwareVersion: $firmwareVersionValue"
-
-                            if (prefs.getBoolean(i, false) == favorite) {
-                                firmwaresAdapter.addDevice(
-                                    FirmwaresData(
-                                        deviceNameValue,
-                                        deviceIconValue,
-                                        firmwareReleaseDateValue,
-                                        firmwareChangelogValue,
-                                        i.toInt()
-                                    )
+                fun getData(favorite: Boolean) {
+                    deviceArrayList.forEachIndexed { index, it ->
+                        if (prefs.getBoolean(it.deviceSource, false) == favorite) {
+                            firmwaresAdapter.addDevice(
+                                FirmwaresData(
+                                    it.name,
+                                    it.icon,
+                                    StringUtils().getLocalFirmwareDate(it.buildTime),
+                                    it.firmware.getString("firmwareVersion").toString(),
+                                    it.deviceSource.toInt(),
+                                    it.productionSource.toInt(),
+                                    appName,
+                                    appVersion.toString()
                                 )
-                                firmwaresAdapter.notifyItemInserted(i.toInt())
-                                deviceListIndex[deviceNameValue] = i.toInt()
-                            }
+                            )
                         }
+                        firmwaresAdapter.notifyItemInserted(index)
+                        deviceListIndex[it.name] = index
                     }
                 }
-                firmwaresProgressBar.visibility = View.GONE
-                firmwaresRefreshLayout.isRefreshing = false
-                getData(true)
-                getData(false)*/
 
-                if (state != null) {
-                    deviceListRecyclerView.layoutManager?.onRestoreInstanceState(state)
-                }
+                getData(true)
+                getData(false)
+                firmwaresProgressBar.visibility = View.GONE
             }
         }
 
@@ -262,8 +185,11 @@ class FeedFragment : Fragment() {
         }
 
         firmwaresRefreshLayout.setOnRefreshListener {
-            deviceListRecyclerView.scrollToPosition(0)
             setData()
+        }
+
+        if (state != null) {
+            deviceListRecyclerView.layoutManager?.onRestoreInstanceState(state)
         }
     }
 
@@ -297,17 +223,5 @@ class FeedFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         firmwaresAdapter.clear()
-    }
-
-    private fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith { it ->
-        when (val value = this[it]) {
-            is JSONArray -> {
-                val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
-                JSONObject(map).toMap().values.toList()
-            }
-            is JSONObject -> value.toMap()
-            JSONObject.NULL -> null
-            else -> value
-        }
     }
 }
