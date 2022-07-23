df171b0c173930496afdca3ec4510a70d49bcce1
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-07-23 12:47:34 +0300

    Update MainActivity.kt

--- app/src/main/java/io/github/keddnyo/midoze/activities/main/MainActivity.kt
+++ app/src/main/java/io/github/keddnyo/midoze/activities/main/MainActivity.kt
@@ -1,10 +1,10 @@
 package io.github.keddnyo.midoze.activities.main
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -13,14 +13,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import io.github.keddnyo.midoze.R
 import io.github.keddnyo.midoze.activities.request.RequestActivity
 import io.github.keddnyo.midoze.local.dataModels.FirmwareData
-import io.github.keddnyo.midoze.remote.AppUpdates
 import io.github.keddnyo.midoze.remote.DozeRequest
 import io.github.keddnyo.midoze.remote.Routes.GITHUB_APP_REPOSITORY
 import io.github.keddnyo.midoze.utils.Display
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private val firmwaresAdapter = FirmwaresAdapter()
@@ -30,16 +29,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        if (android.os.Build.VERSION.SDK_INT >= 21) {
-            AppUpdates(context).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-        }
+
+//        if (android.os.Build.VERSION.SDK_INT >= 21) {
+//            AppUpdates(context).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+//        }
+
         init()
     }
 
     private fun init() {
-        val firmwaresRefreshLayout: SwipeRefreshLayout = findViewById(R.id.firmwaresRefreshLayout)
         val feedProgressBar: ProgressBar = findViewById(R.id.firmwaresProgressBar)
         val emptyResponse: ConstraintLayout = findViewById(R.id.emptyResponse)
 
@@ -53,72 +52,34 @@ class MainActivity : AppCompatActivity() {
         val adapter = firmwaresAdapter
         deviceListRecyclerView.adapter = adapter
 
-        fun isOnline(): Boolean {
-            return DozeRequest().isOnline(context)
-        }
+        Executors.newSingleThreadExecutor().execute {
+            val mainHandler = Handler(Looper.getMainLooper())
 
-        class FetchFirmwareData :
-            AsyncTask<Void?, Void?, Void>() {
-
-            var deviceArrayList: ArrayList<FirmwareData> = arrayListOf()
-
-            @Deprecated("Deprecated in Java")
-            override fun onPreExecute() {
-                super.onPreExecute()
-                firmwaresRefreshLayout.isRefreshing = false
+            mainHandler.post {
                 feedProgressBar.visibility = View.VISIBLE
                 emptyResponse.visibility = View.GONE
             }
 
-            @Deprecated("Deprecated in Java")
-            override fun doInBackground(vararg p0: Void?): Void? {
-                if (isOnline()) {
-                    val zeppDeviceArrayList =
-                        DozeRequest().getFirmwareLatest(context)
-
-                    for (i in zeppDeviceArrayList) {
-                        deviceArrayList.add(i)
-                    }
-                } else {
-                    runOnUiThread {
-                        feedProgressBar.visibility = View.GONE
-                        emptyResponse.visibility = View.VISIBLE
-                    }
+            val deviceArrayList: ArrayList<FirmwareData> = arrayListOf()
+            if (DozeRequest().isOnline(context)) {
+                DozeRequest().getFirmwareLatest(context).forEach { device ->
+                    deviceArrayList.add(device)
                 }
-
-                return null
             }
 
-            @Deprecated("Deprecated in Java")
-            override fun onPostExecute(result: Void?) {
-                super.onPostExecute(result)
-
+            mainHandler.post {
                 firmwaresAdapter.addDevice(deviceArrayList)
-
                 deviceArrayList.forEachIndexed { index, _ ->
                     firmwaresAdapter.notifyItemInserted(index)
                 }
 
-                feedProgressBar.visibility = View.GONE
-
-                if (isOnline() && firmwaresAdapter.itemCount == 0) {
+                if (firmwaresAdapter.itemCount == 0){
                     emptyResponse.visibility = View.VISIBLE
                 }
+
+                feedProgressBar.visibility = View.GONE
             }
         }
-
-        fun setData() {
-            val itemCount = firmwaresAdapter.itemCount
-            firmwaresAdapter.clear()
-            firmwaresAdapter.notifyItemRangeRemoved(0, itemCount)
-            FetchFirmwareData().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-        }
-
-        firmwaresRefreshLayout.setOnRefreshListener {
-            setData()
-        }
-
-        setData()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
