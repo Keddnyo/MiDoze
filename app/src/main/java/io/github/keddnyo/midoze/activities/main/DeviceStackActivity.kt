0fbd88284db2f01a03c911f0ee23c2492a83d392
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-08-01 00:22:00 +0300

    Revert "Update DeviceStackActivity.kt"
    This reverts commit acfb28e001bb35705a0626deb7657b46752bd70b.

--- app/src/main/java/io/github/keddnyo/midoze/activities/main/DeviceStackActivity.kt
+++ app/src/main/java/io/github/keddnyo/midoze/activities/main/DeviceStackActivity.kt
@@ -60,7 +60,7 @@ class DeviceStackActivity : AppCompatActivity() {
         } else {
             GridLayoutManager(
                 this, Display()
-                    .getGridLayoutIndex(this, 250)
+                    .getGridLayoutIndex(this, 200)
             )
         }
 
