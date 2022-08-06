3b89e3d4e0f7ceaa1d1e5d7626df9f3de176d3c0
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-08-06 14:15:36 +0300

    Update AndroidVersion.java

--- app/src/main/java/io/github/keddnyo/midoze/utils/AndroidVersion.java
+++ app/src/main/java/io/github/keddnyo/midoze/utils/AndroidVersion.java
@@ -1,5 +1,5 @@
 package io.github.keddnyo.midoze.utils;
 
 public class AndroidVersion {
-    public Boolean isLollipopOrHigher = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP;
+    public final Boolean isLollipopOrHigher = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP;
 }
