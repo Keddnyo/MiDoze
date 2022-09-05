d0acdad2d273419c2b6935f6e23d9e7127341310
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-09-06 00:14:24 +0300

    Preview optimizations

--- app/src/main/java/io/github/keddnyo/midoze/utils/BitmapCache.kt
+++ app/src/main/java/io/github/keddnyo/midoze/utils/BitmapCache.kt
@@ -3,39 +3,56 @@ package io.github.keddnyo.midoze.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Base64
-import androidx.preference.PreferenceManager
-import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.StringBuilder
 
 class BitmapCache(val context: Context) {
-    private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-    private val editor = prefs.edit()
+    private fun getFileDir(deviceName: String, dialName: String): String {
+        val cacheDir = context.cacheDir
+        return StringBuilder()
+            .append(cacheDir)
+            .append(File.pathSeparator)
+            .append(deviceName)
+            .append(dialName)
+            .toString()
+    }
+
+    private fun Bitmap.smallSize(): Bitmap {
+        val maxSize = 600
+        val outWidth: Int
+        val outHeight: Int
+        val inWidth = this.width
+        val inHeight = this.height
+        if (inWidth > maxSize || inHeight > maxSize) {
+            if (inWidth > inHeight) {
+                outWidth = maxSize
+                outHeight = (inHeight * maxSize) / inWidth
+            } else {
+                outHeight = maxSize
+                outWidth = (inWidth * maxSize) / inHeight
+            }
+        } else {
+            outWidth = inWidth
+            outHeight = inHeight
+        }
+        return Bitmap.createScaledBitmap(this, outWidth, outHeight, false)
+    }
 
     fun encode(deviceName: String, dialName: String, bitmap: Bitmap) {
-        ByteArrayOutputStream().let { stream ->
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
-            val byteArray = Base64.encodeToString(stream.toByteArray(), Base64.DEFAULT)
-            editor.putString(getPreferenceName(deviceName, dialName), byteArray).apply()
+        try {
+            val out = FileOutputStream(File(getFileDir(deviceName, dialName)))
+            bitmap.smallSize().compress(Bitmap.CompressFormat.PNG, 0, out)
+        } catch (e: Exception) {
+
         }
     }
 
     fun decode(deviceName: String, dialName: String): Bitmap? {
-        val preferenceValue = prefs.getString(getPreferenceName(deviceName, dialName), "")
-
-        val byteArray: ByteArray = Base64.decode(preferenceValue, Base64.DEFAULT)
-        val options = BitmapFactory.Options().apply {
-            inJustDecodeBounds = false
-            inPreferredConfig = Bitmap.Config.RGB_565
+        return try {
+            BitmapFactory.decodeFile(getFileDir(deviceName, dialName))
+        } catch (e: Exception) {
+            null
         }
-        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size, options)
-    }
-
-    private fun getPreferenceName(deviceName: String, dialName: String): String {
-        return StringBuilder()
-            .append(deviceName)
-            .append("_")
-            .append(dialName)
-            .toString()
     }
 }
