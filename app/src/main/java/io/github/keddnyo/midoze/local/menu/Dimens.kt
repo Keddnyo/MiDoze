6a710c624f52169987dc922feabd58459bf236ce
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-09-09 13:09:49 +0300

    Fixes

--- app/src/main/java/io/github/keddnyo/midoze/local/menu/Dimens.kt
+++ app/src/main/java/io/github/keddnyo/midoze/local/menu/Dimens.kt
@@ -1,8 +1,6 @@
 package io.github.keddnyo.midoze.local.menu
 
-import android.util.DisplayMetrics
-
 object Dimens {
     const val CARD_GRID_WIDTH = 200
-    const val SWIPE_LAYOUT_REFRESH_LENGTH = 96 * DisplayMetrics.DENSITY_DEFAULT
+    const val SWIPE_LAYOUT_REFRESH_LENGTH = 512
 }
