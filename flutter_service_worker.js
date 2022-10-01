eb2eb81e973078bf0b87180b9d8dee9800b166d8
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-10-01 17:16:10 +0300

    New

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "0eafac00e85ef99e80acb5032487c105",
 "icons/Icon-maskable-192.png": "6a77c3bfb4b42e7849763686381fc51e",
 "icons/Icon-maskable-512.png": "364697a213df392204861dd088c3cd32",
-"index.html": "0e61569dffba204e9d65193066bd4673",
-"/": "0e61569dffba204e9d65193066bd4673",
-"main.dart.js": "c5ab8ce4bb2d132949d5ed64a1e7dfbc",
+"index.html": "5429672f53fe4827c89830d5848dc711",
+"/": "5429672f53fe4827c89830d5848dc711",
+"main.dart.js": "65152a7fca0860679497b3eece84075f",
 "manifest.json": "bb61e72c4031ac52a46cd91e1ec34445",
 "version.json": "eb25b2f87ab0f7f2cb7e2a7dda5fab84"
 };
