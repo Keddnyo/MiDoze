8b43d0139199006149b49a9366bb6e68494fbda2
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2022-10-04 08:38:02 +0300

    New

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "0eafac00e85ef99e80acb5032487c105",
 "icons/Icon-maskable-192.png": "6a77c3bfb4b42e7849763686381fc51e",
 "icons/Icon-maskable-512.png": "364697a213df392204861dd088c3cd32",
-"index.html": "879ea973c714f3c5514c3af53a260c32",
-"/": "879ea973c714f3c5514c3af53a260c32",
-"main.dart.js": "0f2712c7a4a99f5aa862d90a3ecaf313",
+"index.html": "9cab14e099fb57f84146b4015aa29837",
+"/": "9cab14e099fb57f84146b4015aa29837",
+"main.dart.js": "1550208e0b3175e3ecb8dffd138b784b",
 "manifest.json": "bb61e72c4031ac52a46cd91e1ec34445",
 "version.json": "eb25b2f87ab0f7f2cb7e2a7dda5fab84"
 };
