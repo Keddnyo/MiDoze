a3408e9a696a16573fe5f1aef1c93519b3ca62b3
Azat Mironov <65981689+Keddnyo@users.noreply.github.com>
2022-10-30 12:12:01 +0300

    Update MainActivity.kt

--- app/src/main/java/io/github/keddnyo/midoze/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/keddnyo/midoze/ui/main/MainActivity.kt
@@ -3,12 +3,24 @@ package io.github.keddnyo.midoze.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import io.github.keddnyo.midoze.ui.theme.MiDozeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // TODO: Create content
+            MiDozeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // TODO: Set content
+                }
+            }
         }
     }
 }
