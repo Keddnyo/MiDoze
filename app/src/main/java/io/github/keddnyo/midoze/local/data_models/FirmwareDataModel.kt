22192191a12bccfaafeb7fbd4c620988dc8a2bd2
Azat Mironov <65981689+Keddnyo@users.noreply.github.com>
2022-10-31 08:09:38 +0300

    Initial paging sourcing code added

---
+++ app/src/main/java/io/github/keddnyo/midoze/local/data_models/FirmwareDataModel.kt
@@ -0,0 +1,8 @@
+package io.github.keddnyo.midoze.local.data_models
+
+data class FirmwareDataModel (
+    val firmwareVersion: String,
+    val firmwareUrl: String,
+    val resourceVersion: String,
+    val resourceUrl: String,
+)
