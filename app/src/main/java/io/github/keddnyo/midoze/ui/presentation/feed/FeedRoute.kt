06a19ffc376dfd4ac9405ac195d53a7d5be3e623
Azat Mironov <65981689+Keddnyo@users.noreply.github.com>
2022-11-19 18:23:00 +0300

    Fixes

--- app/src/main/java/io/github/keddnyo/midoze/ui/presentation/feed/FeedRoute.kt
+++ app/src/main/java/io/github/keddnyo/midoze/ui/presentation/feed/FeedRoute.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -31,8 +28,8 @@ fun FeedRoute(
     coroutineScope: CoroutineScope
 ) {
 
-    val firmwareList = viewModel.firmwareList.collectAsLazyPagingItems()
     val context = LocalContext.current
+    val firmwareList = viewModel.firmwareList.collectAsLazyPagingItems()
 
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
